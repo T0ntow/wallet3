b30ca16e8f6065b6bbf41f03e2f11225e5629466
Wellington Batista <wellingtonbs109@gmail.com>
2025-03-20 10:35:21 -0300

    fix: mes das despesas

--- src/app/components/month-navigation/month-navigation.component.ts
+++ src/app/components/month-navigation/month-navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, EventEmitter, Output, Renderer2 } from '@angular/core';
 import * as moment from 'moment';
 
 @Component({
@@ -7,11 +7,37 @@ import * as moment from 'moment';
   styleUrls: ['./month-navigation.component.scss'],
 })
 export class MonthNavigationComponent {
-  // Initialize with the current month
   currentMonth = moment(); // Mês atual
-  
-  // Evento para emitir o mês selecionado
+  mesSelecionado!: number;
+
+  mostrarDatePicker: boolean = false;
+  anoSelecionado: number = moment().year();
+  mesesCompletos: string[] = [
+    'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+    'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
+  ];
+
+  mesesAbreviados: string[] = [
+    'JAN.', 'FEV.', 'MAR.', 'ABR.', 'MAI.', 'JUN.',
+    'JUL.', 'AGO.', 'SET.', 'OUT.', 'NOV.', 'DEZ.'
+  ];
+  meses: any;
   @Output() monthChanged = new EventEmitter<string>();
+  constructor(
+    private elRef: ElementRef,
+    private renderer: Renderer2,
+  ) { }
+
+  ngOnInit() {
+    const now = moment();
+    this.mesSelecionado = now.month(); // Inicializa com o mês atual
+
+    this.renderer.listen('document', 'click', (event: MouseEvent) => {
+      if (this.mostrarDatePicker && !this.elRef.nativeElement.contains(event.target)) {
+        this.toggleDatePicker();
+      }
+    });
+  }
 
   // Getters for month labels
   get selectedMonthLabel() {
@@ -32,13 +58,17 @@ export class MonthNavigationComponent {
 
   // Navigate to the previous month (button click)
   prevMonth() {
-    this.currentMonth = this.currentMonth.subtract(1, 'months');
-    this.emitCurrentMonth(); // Emite o mês atualizado
+    this.currentMonth = this.currentMonth.subtract(1, 'months');  // Subtrai um mês do currentMonth
+    this.mesSelecionado = this.currentMonth.month();  // Obtém o índice do mês após a subtração
+    this.anoSelecionado = this.currentMonth.year();  // Obtém o índice do mês após a subtração
+    this.emitCurrentMonth();  // Emite o mês atualizado
   }
 
   // Navigate to the next month (button click)
   nextMonth() {
     this.currentMonth = this.currentMonth.add(1, 'months');
+    this.mesSelecionado = this.currentMonth.month();  // Obtém o índice do mês após a subtração
+    this.anoSelecionado = this.currentMonth.year();  // Obtém o índice do mês após a subtração
     this.emitCurrentMonth(); // Emite o mês atualizado
   }
 
@@ -50,5 +80,37 @@ export class MonthNavigationComponent {
   // Select next month label click
   selectNextMonth() {
     this.nextMonth(); // Reuse the nextMonth method
+  }
+
+  toggleDatePicker() {
+    this.mostrarDatePicker = !this.mostrarDatePicker;
+  }
+
+  mudarAno(valor: number) {
+    this.anoSelecionado += valor;
+  }
+
+  selecionarMes(indice: number) {
+    this.mesSelecionado = indice;
+    this.currentMonth = moment().year(this.anoSelecionado).month(indice); // Atualiza o mês selecionado
+    this.monthChanged.emit(this.currentMonth.format('YYYY-MM')); // Emite o evento corretamente
+    this.toggleDatePicker();
+  }
+
+  mesAtual() {
+    const now = moment();
+    this.mesSelecionado = now.month();  // Atualiza o índice do mês selecionado
+    this.anoSelecionado = now.year();  // Atualiza o ano selecionado
+
+    this.currentMonth = now;  // Atualiza o momento com o mês atual
+    this.monthChanged.emit(this.currentMonth.format('YYYY-MM')); // Emite o evento corretamente
+
+    this.toggleDatePicker();  // Alterna o date picker
+  }
+
+
+  cancelar() {
+    this.toggleDatePicker();
+    console.log('Seleção de data cancelada');
   }
 }
